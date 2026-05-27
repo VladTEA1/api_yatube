d985023b2c2ee347e8f290bf410a7f355e1d9eb4
VladTEA1 <161972729+VladTEA1@users.noreply.github.com>
2026-05-27 14:56:07 +0300

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -7,7 +7,11 @@ from .views import CommentViewSet, GroupViewSet, PostViewSet
 router = DefaultRouter()
 router.register('posts', PostViewSet, basename='posts')
 router.register('groups', GroupViewSet, basename='groups')
-router.register('posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comment')
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comment'
+)
 
 urlpatterns = [
     path('v1/api-token-auth/', views.obtain_auth_token),
